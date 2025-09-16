ceee787c0530cd7604c10f67901818393cb646a9
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2025-09-16 21:14:43 +0530

    deployement ready

--- Email/Email/settings.py
+++ Email/Email/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-f42f0-wq@%#!!8g#ytro8^ajgz=y9-mjl3sy(5(^-!io+s#=5@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
---
+++ Email/staticfiles/js/Home/main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.querySelector('.contact-form');
+    if (contactForm) {
+        contactForm.classList.add('loaded');
+    }
+});
