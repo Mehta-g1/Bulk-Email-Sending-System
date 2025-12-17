85a7479c68d673b388ccdb29b34600936b4d17ea
Vikash Kumar Mehta <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2025-12-17 13:05:44 +0530

    comma fixed

--- Email/Email/settings.py
+++ Email/Email/settings.py
@@ -68,7 +68,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validtion.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     }, 
 ]
 
